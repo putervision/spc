6eeb51580689b9d8d416531d9590f409b831d357
Lucas Armstrong <lucas@throneit.com>
2025-02-25 20:08:58 -0500

    1.1.12

--- bin/cli.js
+++ bin/cli.js
@@ -79,7 +79,7 @@ async function main() {
     case '-v':
       console.log(`${CLI_NAME} v${VERSION}`);
       return;
-    
+
     case '--create-sums':
     case '-cs':
       return await scanDirectory(process.cwd(), true);
